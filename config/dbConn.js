998c9b82f569f8f91440ce7323cf386286c10d0c
sumit8294 <sumitrathore8294@gmail.com>
2024-03-07 14:27:46 +0530

    Adding new database url

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
-const DATABASE_URI = 'mongodb+srv://blink-db-social:HvOjYzJ5nSb8HyJ4@cluster0.douur2b.mongodb.net/blink-db?retryWrites=true&w=majority';
+
+//old uri
+//const DATABASE_URI = 'mongodb+srv://blink-db-social:HvOjYzJ5nSb8HyJ4@cluster0.douur2b.mongodb.net/blink-db?retryWrites=true&w=majority';
+
+//new cluster uri2024
+const DATABASE_URI = 'mongodb+srv://blink2024:NXFy38CeXrZecIIV@cluster0.z5rikpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const options = {
   useNewUrlParser: true,
